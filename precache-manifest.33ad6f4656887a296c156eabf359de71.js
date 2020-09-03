be379b6623b892b836f70ca6f00b8f437123fefa
Anita Chang <huilong494@gmail.com>
2020-09-03 10:37:29 -0700

    Updates

---
+++ precache-manifest.33ad6f4656887a296c156eabf359de71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f12548607c498c2f5cc63a7225d2af06",
+    "url": "/yoboline_interview_react/index.html"
+  },
+  {
+    "revision": "66f1044f7e1df9be9996",
+    "url": "/yoboline_interview_react/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "37a5469ba798c323e14f",
+    "url": "/yoboline_interview_react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "66f1044f7e1df9be9996",
+    "url": "/yoboline_interview_react/static/js/2.fe438db8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/yoboline_interview_react/static/js/2.fe438db8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37a5469ba798c323e14f",
+    "url": "/yoboline_interview_react/static/js/main.2e2b085c.chunk.js"
+  },
+  {
+    "revision": "686ca2275904a07f0528",
+    "url": "/yoboline_interview_react/static/js/runtime-main.bf345b8d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.76f5362fb8ab4403e542726e02927f35.js"
+  "/yoboline_interview_react/precache-manifest.33ad6f4656887a296c156eabf359de71.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/yoboline_interview_react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
