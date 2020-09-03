fc4e2583e83f23c5317d31e4d305225cb112860b
Anita Chang <huilong494@gmail.com>
2020-09-03 10:14:54 -0700

    Updates

---
+++ precache-manifest.76f5362fb8ab4403e542726e02927f35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55fff9e80fc7150c2aa02971be5ae7d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66f1044f7e1df9be9996",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "45eb7b5aa42c19b668dc",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "66f1044f7e1df9be9996",
+    "url": "/static/js/2.fe438db8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.fe438db8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45eb7b5aa42c19b668dc",
+    "url": "/static/js/main.9fcea443.chunk.js"
+  },
+  {
+    "revision": "08bd9a67a0495b5a07e8",
+    "url": "/static/js/runtime-main.690a2c58.js"
+  }
+]);
